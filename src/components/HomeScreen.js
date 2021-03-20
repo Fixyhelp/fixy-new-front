386b3a3f3f6e76124c483f250aeb7bfc4923acf1
wordfreak <ladimals@gmail.com>
2021-03-20 16:24:20 +0100

    done

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import cookie from 'react-cookies';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import { 
     InputGroup,
     InputGroupText,
@@ -16,16 +17,26 @@ import {
 import SettingsIcon from "@material-ui/icons/Settings";
 import SearchIcon from "@material-ui/icons/Search";
 import CloseIcon from "@material-ui/icons/Close";
+import { blue, grey } from "@material-ui/core/colors";
 
 const HomeScreen = () => {
 
     const darkMode = 'isDarkMode';
     const [modals, modalSet] = useState({open: false});
+    const [locModals, locModalSet] = useState({open: false});
     const [isDarkMode, setIsDarkMode] = useState(false);
+    const [queryStr, setQueryStr] = useState("");
+    const [loca, setLoca] = useState(null);
 
     const toggle = () => {
         modalSet({
           open: !modals.open
+        });
+    }
+
+    const toggleLoc = () => {
+        locModalSet({
+          open: !locModals.open
         });
     }
 
@@ -54,6 +65,10 @@ const HomeScreen = () => {
         }
     }
 
+    const handleSubmit = () =>{
+        toggleLoc();
+    }
+
     return (
         <div className="text-center mn-h-1">
             <div className="container mn-h-1 mx-auto my-5">
@@ -80,7 +95,7 @@ const HomeScreen = () => {
                                 <FormCheckbox
                                     toggle
                                     checked={isDarkMode}
-                                    onChange={() => {
+                                    onChange={(e) => {
                                         handleDarkModeToggle();
                                     }}>
                                     Enable Dark Mode
@@ -89,13 +104,51 @@ const HomeScreen = () => {
                         </p>
                     </ModalBody>
                 </Modal>
+                <Modal size="md" style={{ height: 30 }} centered={true} open={locModals.open} toggle={() => {locModalSet({open: true})}}>
+                    <ModalBody className="p-2">
+                        <h6 className="d-inline" style={{fontSize: 12}}>Select a prefered Location?</h6>
+                        <div className="d-inline ml-5">
+                            <a style={{fontSize: 10, marginRight: 6, color: grey[500], cursor: "pointer"}}>No</a>
+                            <a 
+                                style={{color: blue[300], fontSize: 10, cursor: "pointer"}}
+                                onClick={() => {
+                                    document.getElementById('vib').style.display = "block";
+                                }}
+                                >Yes</a>
+                        </div>
+                        
+                        <div style={{display: "none", marginTop: 10}} id="vib">
+                            <InputGroup size="lg">
+                                <GooglePlacesAutocomplete
+                                    apiKey="AIzaSyALbxSOuepEufVuCuZs9KksXWocvsCiIJA"
+                                    selectProps={{
+                                        loca,
+                                        onChange: setLoca,
+                                    }}
+                                />
+                                <Button theme="light" size="sm" onClick={() => {
+
+                                }}
+                                >Done</Button>
+                            </InputGroup>
+                        </div>
+                    </ModalBody>
+                </Modal>
                 <h3 className="text-bold">Fixy</h3>
                 <div className="container mt-5 w-sm-100">
                     <InputGroup>
-                        <FormInput placeholder="What do you need ?" />
+                        <FormInput placeholder="What do you need ?" onChange={(e) => {
+                            setQueryStr(e.target.value);
+                        }} onKeyUp={(e) => {
+                            if(e.keyCode === 13){
+                                handleSubmit();
+                            }
+                        }}/>
                         <InputGroupAddon type="append">
                             <InputGroupText>
-                                <SearchIcon />
+                                <SearchIcon onClick={() => {
+                                    handleSubmit();
+                                }}/>
                             </InputGroupText>
                         </InputGroupAddon>
                     </InputGroup>
